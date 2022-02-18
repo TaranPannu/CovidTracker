d0c5686c1bd2cd0db277e41be47a6fbf21e03712
Taranpreet Singh <taranpannu407@gmail.com>
2022-02-17 20:51:59 -0330

    Covid_Tracker

---
+++ app/src/main/java/com/example/mac/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.mac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public static final String EXTRA_TEXT = "com.example.application.example.EXTRA_TEXT";
+    private RequestQueue mQueue;
+private TextView tv;
+private RequestQueue m1Queue;
+private Button btn;
+private  TextView tv1;
+private TextView tv2;
+private TextView tv3;
+private int count=0;
+private int count1=0;
+private int count2=0;
+private String date="";
+    //public ListView liss;
+String s="";
+    String sex="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.sp);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        mQueue = Volley.newRequestQueue(this);
+      tv=findViewById(R.id.vv);
+        tv1=findViewById(R.id.vv2);
+        tv2=findViewById(R.id.vv3);
+        tv3=findViewById(R.id.vv4);
+
+        btn=findViewById(R.id.button);
+      m1Queue=Volley.newRequestQueue(this);
+      btn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              count=0;
+                      count1=0;
+                      count2=0;
+              jsonparse();
+          }
+      });
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        String url = "https://api.opencovid.ca/summary";
+        // tv.append("9inside9");
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //   tv.append(response+"");
+
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("summary");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject employee = jsonArray.getJSONObject(i);
+                                String firstName = employee.getString("province");
+                                if(text.equals(firstName)) {
+                                    s= employee.getString("province") +','+employee.getString("date")+ ',' + employee.getString("cases") +
+                                            ','+ employee.getString("cumulative_deaths");
+//tv.setText(s);
+                                openActivity2(s);
+                                }}
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    public void openActivity2(String st) {
+//String ss=jsonparse(st);
+        Intent intent = new Intent(this,Slide2.class);
+        intent.putExtra(EXTRA_TEXT,st);
+        startActivity(intent);
+    }
+  private void jsonparse(){
+
+      String url = "https://api.opencovid.ca/summary";
+      // tv.append("9inside9");
+
+      JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+              new Response.Listener<JSONObject>() {
+                  @Override
+                  public void onResponse(JSONObject response) {
+                      //   tv.append(response+"");
+
+                      try {
+                          JSONArray jsonArray = response.getJSONArray("summary");
+                          for (int i = 0; i < jsonArray.length(); i++) {
+                              JSONObject employee = jsonArray.getJSONObject(i);
+                              //String count=employee.getString("active_cases_change");
+                             count += employee.getInt("cases");
+                            count1+=employee.getInt("deaths");
+                            count2+=employee.getInt("active_cases");
+                            date=employee.getString("date");
+                             }
+                      tv1.setText("                  Cases Reported on "+date+":: "+count);
+                         tv2.setText("Deaths Reported: "+count1);
+                          tv3.setText("Total active cases: "+count2);
+                      } catch (JSONException e) {
+                          e.printStackTrace();
+                      }
+                  }
+              }, new Response.ErrorListener() {
+          @Override
+          public void onErrorResponse(VolleyError error) {
+              error.printStackTrace();
+          }
+      });
+
+      m1Queue.add(request);    }
+
+}
---
+++ app/src/main/java/com/example/mac/Slide2.java
@@ -0,0 +1,46 @@
+package com.example.mac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Slide2 extends AppCompatActivity {
+    private TextView tv;
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
+    public String s1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide2);
+        tv=findViewById(R.id.mm);
+tv1=findViewById(R.id.mm1);
+tv2=findViewById(R.id.mm2);
+tv3=findViewById(R.id.mm3);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+      String m[]=text.split(",");
+tv.setText("PROVINCE: "+m[0]);
+tv1.setText("Data reported on: "+m[1]);
+if(m[2].charAt(0)=='-')
+    m[2]='0'+"";
+tv2.setText("Cases: "+m[2]);
+tv3.setText("Total Deaths: "+m[3]);
+    }
+}
